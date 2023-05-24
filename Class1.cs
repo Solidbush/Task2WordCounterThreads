1dbf7a04065bb0cf0ecd2af575067636eebb7dd8
Alex Gavrilov <solidbush@mail.ru>
2023-05-25 00:46:39 +0300

    .dll

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+
+namespace FileReaderLib
+{
+    public class WordCounter
+    {
+        private static readonly char[] delimiterChars = { ' ', ',', '.', ':', '!', '?', '-', '\t' };
+        private static readonly char newLine = '\n';
+        private static readonly string nullString = "";
+        private const int CommonValue = 1;
+        private const int Incriment = 1;
+        private static Dictionary<string, int> wordCountDictionary = new Dictionary<string, int>();
+
+        private Dictionary<string, int> CountWords(string text)
+        {
+            string[] lines = text.Split(newLine);
+            foreach(var line in lines)
+            {
+                string[] words = line.Split(delimiterChars);
+                foreach (string word in words)
+                {
+                    var wordToLower = word.ToLower().Trim();
+                    if (wordToLower == nullString)
+                        continue;
+                    if (wordCountDictionary.ContainsKey(wordToLower))
+                        wordCountDictionary[wordToLower] += Incriment;
+                    else
+                        wordCountDictionary.Add(wordToLower, CommonValue);
+                }
+            }
+
+            return wordCountDictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        public Dictionary<string, int> CountWordsThread(string text)
+        {
+            string[] lines = text.Split(newLine);
+            foreach (string line in lines)
+            {
+                new Thread(() =>
+                {
+                    System.Action action = () =>
+                    {
+                        string[] words = line.Split(delimiterChars);
+                        foreach(string word in words)
+                        {
+                            var wordToLower = word.ToLower().Trim();
+                            if (wordToLower == nullString)
+                                continue;
+                            if (wordCountDictionary.ContainsKey(wordToLower))
+                                wordCountDictionary[wordToLower] += Incriment;
+                            else
+                                wordCountDictionary.Add(wordToLower, CommonValue);
+                        }
+                    };
+
+                }).Start();
+            }
+
+            return wordCountDictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+    }
+}
